aec125584628c4ed9e422343a517315d117427a8
Poinkk815 <78383030+Poinkk815@users.noreply.github.com>
2021-03-04 09:32:41 -0600

    Add files via upload

---
+++ Poinkk-Chat-Commands/client/client.lua
@@ -0,0 +1,62 @@
+------------------------------------------------------------------------------------------------------------
+--                                                                                                        --
+--  ____           _           _  _    _  _     __                                                    _   --
+-- |  _ \   ___   (_)  _ __   | |/ / | |/ /   / __| ___    _ __ ___    _ __ ___    __ _   _ __     __| |  --
+-- | |_) | / _ \  | | | '_ \  | / /  | / /   | |    / _ \ |  _   _ \  |  _   _ \  / _  | |  _  \  / _  |  --
+-- |  __/ | (_) | | | | | | | | \ \  | \ \   | |__ | (_)| | | | | | | | | | | | || |_| | | | | | | (_| |  --
+-- | |     \___/  | | |_| |_| |_|\_\ |_|\_\   \___| \___/ |_| |_| |_| |_| |_| |_| \__ _| |_| |_|  \__ _|  --
+--                                   Made By: poinkk815#4094                                              --
+------------------------------------------------------------------------------------------------------------
+Citizen.CreateThread(function()
+    TriggerEvent('chat:addSuggestion', '/me', 'Send a third person message (Proximity)', {
+        {name = "Action", help = "action."}
+    })
+    
+    TriggerEvent('chat:addSuggestion', '/do', 'Send an action message (Global)', {
+        {name = "Action", help = "action."}
+    })
+    
+    TriggerEvent('chat:addSuggestion', '/twt', 'Send a tweet (Global)', {
+        {name = "Action", help = "action."}
+    })
+    
+    TriggerEvent('chat:addSuggestion', '/ooc', 'Send an out of character message (Global)', {
+        {name = "Action", help = "action."}
+    })
+    
+    TriggerEvent('chat:addSuggestion', '/darkweb', 'Send an anymous message (Global)', {
+        {name = "Action", help = "action."}
+    })
+
+    TriggerEvent('chat:addSuggestion', '/911', 'Send a 911 call (Global)', {
+        {name = "Action", help = "action."}
+    })
+
+    TriggerEvent('chat:addSuggestion', '/showid', 'Show someone your ID name (Proximity)', {
+        {name = "Action", help = "action."}
+    })
+end)
+
+-- /me command --
+RegisterNetEvent('SendProximityMe')
+AddEventHandler('SendProximityMe', function(id, name, message)
+    local myID = PlayerId()
+    local pID = GetPlayerFromServerId(id)
+    if pID == myID then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .. "^7: " .. "^r " .. message)
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myID)), GetEntityCoords(GetPlayerPed(pID)), true) < 19.999 then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .. "^7: " .. "^r " .. message)
+    end
+end)
+
+-- /showid command --
+RegisterNetEvent('SendProximityShowid')
+AddEventHandler('SendProximityShowid', function(id, name, message)
+    local myID = PlayerId()
+    local pID = GetPlayerFromServerId(id)
+    if pID == myID then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*Showid | ^7" .. name .. "^7: " .. "^r " .. message)
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myID)), GetEntityCoords(GetPlayerPed(pID)), true) < 19.999 then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*Showid | ^7" .. name .. "^7: " .. "^r " .. message)
+    end
+end)
---
+++ Poinkk-Chat-Commands/server/server.lua
@@ -0,0 +1,48 @@
+------------------------------------------------------------------------------------------------------------
+--                                                                                                        --
+--  ____           _           _  _    _  _     __                                                    _   --
+-- |  _ \   ___   (_)  _ __   | |/ / | |/ /   / __| ___    _ __ ___    _ __ ___    __ _   _ __     __| |  --
+-- | |_) | / _ \  | | | '_ \  | / /  | / /   | |    / _ \ |  _   _ \  |  _   _ \  / _  | |  _  \  / _  |  --
+-- |  __/ | (_) | | | | | | | | \ \  | \ \   | |__ | (_)| | | | | | | | | | | | || |_| | | | | | | (_| |  --
+-- | |     \___/  | | |_| |_| |_|\_\ |_|\_\   \___| \___/ |_| |_| |_| |_| |_| |_| \__ _| |_| |_|  \__ _|  --
+--                                   Made By: poinkk815#4094                                              --
+------------------------------------------------------------------------------------------------------------
+
+
+-- /me command --
+RegisterCommand('me', function(source, args, user)
+    local name = GetPlayerName(source)
+    TriggerClientEvent("SendProximityMe", -1, source, name, table.concat(args, " "))
+end, false)
+
+-- /do Command --
+RegisterCommand('do', function(source, args, user)
+    local name = GetPlayerName(source)
+    TriggerClientEvent('chatMessage', -1, "^1^*Do | ^7" .. GetPlayerName(source) .. "^r", {128, 128, 128}, table.concat(args, " "))
+end, false)
+
+-- /showid command--
+RegisterCommand('gme', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^1^*Showid | ^7" .. GetPlayerName(source) .. "^r", {128, 128, 128}, table.concat(args, " "))
+end, false)
+
+-- /twt command --
+RegisterCommand('twt', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^5^*Twitter | ^7@" .. GetPlayerName(source) .. "^r", {30, 144, 255}, table.concat(args, " "))
+end, false)
+
+-- /occ command --
+RegisterCommand('ooc', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^4^*OOC | ^7" .. GetPlayerName(source) .. "^r", {128, 128, 128}, table.concat(args, " "))
+end, false)
+
+-- /darkweb --
+RegisterCommand('darkweb', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^*Darkweb | ^7@Anonymous ^r", {0, 0, 0}, table.concat(args, " "))
+end, false)
+
+-- /911 command --
+RegisterCommand('911', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^1^*911 | ^7" .. GetPlayerName(source) .. "^r", {128, 128, 128}, table.concat(args, " "))
+end, false)
+
