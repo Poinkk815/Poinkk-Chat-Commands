a03962da9939e1740472b55356413b08a1a5a130
Poinkk815 <78383030+Poinkk815@users.noreply.github.com>
2021-04-18 20:50:45 -0500

    Add files via upload

---
+++ Poinkk-Chat-Commands/config.lua
@@ -0,0 +1,21 @@
+------------------------------------------------------------------------------------------------------------
+--                                                                                                        --
+--  ____           _           _  _    _  _     __                                                    _   --
+-- |  _ \   ___   (_)  _ __   | |/ / | |/ /   / __|  ___   _ __ ___    _ __ ___    __ _   _ __     __| |  --
+-- | |_) | / _ \  | | | '_ \  | / /  | / /   | |    / _ \ |  _   _ \  |  _   _ \  / _  | |  _  \  / _  |  --
+-- |  __/ | (_) | | | | | | | | \ \  | \ \   | |__ | (_)| | | | | | | | | | | | || |_| | | | | | | (_| |  --
+-- | |     \___/  | | |_| |_| |_|\_\ |_|\_\   \___| \___/ |_| |_| |_| |_| |_| |_| \__ _| |_| |_|  \__ _|  --
+--                                   Made By: poinkk815#4094                                              --
+------------------------------------------------------------------------------------------------------------
+
+Config = {}
+
+Config = {
+    commandName = "staff",
+    -- This is the command that you will type into chat to execute the script.
+
+    restricCommand = true
+    -- Setting this to false will allow anyone in the server to use the command. 
+    -- If you set it to true you will need to add a ace perm to allow people to use it.
+    -- Such as add_ace [GROUP] command.[commandName] allow
+}
